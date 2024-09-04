413d1e527bc11e0a5c964ccabd007473570ea082
s1gr1d <sigrid.huemer@posteo.at>
2024-09-04 12:22:46 +0200

    feat(sentry): Add error test page

---
+++ pages/test-error.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useFetch } from "#imports";
+
+const triggerError = () => {
+	throw new Error("Nuxt Button Error");
+};
+const fetchData = async () => {
+	await useFetch("/api/testError");
+};
+</script>
+
+<template>
+	<div>
+		<button id="errorBtn" @click="triggerError">Trigger Client Error</button>
+		<br />
+		<button @click="fetchData">Fetch Server Data</button>
+	</div>
+</template>
