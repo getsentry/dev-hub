4092673419f5c41f2d35c4cca42740489ec54386
s1gr1d <sigrid.huemer@posteo.at>
2024-09-04 12:23:48 +0200

    feat(sentry): Enable debug on server

--- public/instrument.server.mjs
+++ public/instrument.server.mjs
@@ -3,6 +3,7 @@ import * as Sentry from "@sentry/nuxt";
 // Only run `init` when DSN is available
 if (process.env.SENTRY_DSN) {
 	Sentry.init({
-		dsn: "https://5d2dc2cbcf5120c343569d1d3d87e071@o447951.ingest.us.sentry.io/4507825948983296"
+		dsn: "https://5d2dc2cbcf5120c343569d1d3d87e071@o447951.ingest.us.sentry.io/4507825948983296",
+		debug: true
 	});
 }
